c73cc73b927c820d44942eed8ea1b97423b371c4
ANSAR KAMAL <152076853+Haru-bit-code@users.noreply.github.com>
2026-05-04 22:30:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -42,4 +42,4 @@ def handle_join(data):
     }, broadcast=True)
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=10000, debug=True)
+    socketio.run(app, host="0.0.0.0", port=10000, debug=True, allow_unsafe_werkzeug=True)
